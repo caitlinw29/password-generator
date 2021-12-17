b0299044bc43c2e276a35c688e05418834b28f1a
Caitlin Wall <caitlinwall96@gmail.com>
2021-12-16 20:12:17 -0500

    Refactor for SOC - separate functions a bit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,28 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
+//options for password characters
+var specialChar = ["\!", "\"", "\#", "\$", "\%", "\&", "\'", "\(", "\)", "\*", "\+", "\,", "\-", "\.", "\/", "\:", "\;", "\<", "\=", "\>", "\?", "\@", "\[", "\\", "\]", "\^", "\_", "\`", "\{", "\|", "\}", "\~"];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var lowerABC = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperABC = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+//Initialize confirmation variables
+var includeSC;
+var includeNum;
+var includeLowABC;
+var includeUpABC;
+//array placeholder for user selected number of characters
+var password = [];
+
+//Confirmations to check for character types 
+function confirmations() {
+  includeSC = window.confirm("Click OK to confirm including special characters.");
+  includeNum = window.confirm("Click OK to confirm including numeric characters.");
+  includeLowABC = window.confirm("Click OK to confirm including lowercase characters.");
+  includeUpABC = window.confirm("Click OK to confirm including uppercase characters.");
+};
 
 function generatePassword() {
-  //options for password characters
-  var specialChar = ["\!", "\"", "\#", "\$", "\%", "\&", "\'", "\(", "\)", "\*", "\+", "\,", "\-", "\.", "\/", "\:", "\;", "\<", "\=", "\>", "\?", "\@", "\[", "\\", "\]", "\^", "\_", "\`", "\{", "\|", "\}", "\~"];
-  var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  var lowerABC = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-  var upperABC = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
-  //Initialize confirmation variables
-  var includeSC;
-  var includeNum;
-  var includeLowABC;
-  var includeUpABC;
-
-  //array placeholder for user selected number of characters
-  var password = [];
-
   //Prompt for number of characters in password
   var numberOfCharacters = prompt("How many characters would you like your password to contain? Choose between 8-128");
-
 
   //Validation for numberOfCharacters
   if (numberOfCharacters === null) {
@@ -28,46 +31,19 @@ function generatePassword() {
   } else if (!numberOfCharacters) {
     //If user hits okay without typing anything, prompt to provide a number
     alert("You need to input a number to make a password");
-    generatePassword();
+    return;
   } else if (numberOfCharacters < 8 || numberOfCharacters > 128) {
         //if number is not in correct range, prompt to try again. 
-        numberOfCharacters = prompt("Please choose a number between 8-128");
-
-        //Need to revalidate everything
-        if (numberOfCharacters === null) {
-          return;
-        } else if (!numberOfCharacters) {
-          alert("You need to input a number to make a password");
-          generatePassword();
-        } else if (numberOfCharacters < 8 || numberOfCharacters > 128) {
-          //if number is not in correct range, prompt to try again
-            alert("You need to choose an appropriate number");
-            generatePassword();
-        } else if (lowerABC.includes(numberOfCharacters) || upperABC.includes(numberOfCharacters) || specialChar.includes(numberOfCharacters)) {
-          //If user inputs a letter or special character, provide error message.
-          alert("You need to use a number");
-          generatePassword();
-        } else {
-          //Check character types
-          confirmations();  
-        }
+        alert("Please choose a number between 8-128");
+        return;
   } else if (lowerABC.includes(numberOfCharacters) || upperABC.includes(numberOfCharacters) || specialChar.includes(numberOfCharacters)) {
     //If user inputs a letter or special character, provide error message.
     alert("You need to use a number");
-    generatePassword();
+    return;
   } else {
     //If user inputs an appropriate number, check which character types they would like to include
     confirmations();
   };
-
-  //Confirmations to check for character types 
-  function confirmations() {
-    includeSC = window.confirm("Click OK to confirm including special characters.");
-    includeNum = window.confirm("Click OK to confirm including numeric characters.");
-    includeLowABC = window.confirm("Click OK to confirm including lowercase characters.");
-    includeUpABC = window.confirm("Click OK to confirm including uppercase characters.");
-  };
-
 
   //Create password array by merging character arrays with concat  
   //includes all four types in selection
@@ -111,9 +87,8 @@ function generatePassword() {
   } //If 4 negatives, prompt user to select at least one type
   else {
     alert("You must choose at least one type of character");
-    generatePassword();
+    return;
   };
-
 
   //Create Randomized Password
   //Empty array for randomized password characters
@@ -166,6 +141,5 @@ function writePassword() {
 
 };
 
-// Add event listener to generate button
+// Add event listener to "generate" button
 generateBtn.addEventListener("click", writePassword);
-
